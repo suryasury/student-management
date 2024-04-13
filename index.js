dbe532d2d227b9facd8290b9b153ecb090f2d8ce
surya <surya11981.sa@gmail.com>
2024-04-13 21:30:10 +0530

    removed unused code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const passport = require("passport");
-const httpStatus = require("http-status");
 const routes = require("./routes");
-// const { jwtStrategy } = require("./config/passport");
-// const { errorConverter, errorHandler } = require("./middlewares/error");
-// const ApiError = require("./helper/ApiError");
 
 require("dotenv").config();
 
@@ -17,39 +12,11 @@ const app = express();
 app.use(cors());
 app.options("*", cors());
 
-// app.use(express.static(`${process.env.PWD}/public`));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//aws sdk sm process.env = {}
-
-// jwt authentication
-// app.use(passport.initialize());
-// passport.use("jwt", jwtStrategy);
-
-// app.get("/", async (req, res) => {
-//   res.status(200).send("Congratulations! API is working!");
-// });
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log("Connected to port ", process.env.PORT);
 });
-
-// send back a 404 error for any unknown api request
-// app.use((req, res, next) => {
-//   next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
-// });
-
-// convert error to ApiError, if needed
-// app.use(errorConverter);
-
-// handle error
-// app.use(errorHandler);
-// const db = require("./models");
-
-// Uncomment this line if you want to sync database model
-// db.sequelize.sync()
-
-// module.exports = app;
